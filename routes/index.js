a27e0366831c41143d6aee9c8decda26c6838df7
aaymenb <aymen1996ben@gmail.com>
2026-05-01 19:05:03 +0200

    RIPRO3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import AppController from '../controllers/AppController.js';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
+// Route pour vérifier l'état des services (Redis et DB)
 router.get('/status', AppController.getStatus);
+
+// Route pour obtenir les statistiques (nb de users et de files)
 router.get('/stats', AppController.getStats);
+
+// Route pour créer un nouvel utilisateur
+router.post('/users', UsersController.postNew);
 
 export default router;
